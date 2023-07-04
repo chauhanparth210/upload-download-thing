007a92582b88dc70df242875191defd09f92c6b1
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-04 17:06:33 +0530

    serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppResolver } from './app.resolver';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import * as typeOrmConfigration from 'orm.config';
+import { Module } from "@nestjs/common";
+import { AppResolver } from "./app.resolver";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
+import * as typeOrmConfigration from "orm.config";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfigration),
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
     }),
   ],
   providers: [AppService, AppResolver],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "dotenv";
+import { graphqlUploadExpress } from "graphql-upload";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 config();
 
+const BUCKET_DIRECTORY = "/file_bucker";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(graphqlUploadExpress());
+  app.useStaticAssets(join(__dirname, "../..", BUCKET_DIRECTORY), {
+    prefix: `/${BUCKET_DIRECTORY}`,
+  });
   await app.listen(process.env.APPLICATION_PORT);
 }
 bootstrap();
