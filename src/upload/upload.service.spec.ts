67d53c71b13336cae8d71f197f34d7af0f355f79
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-05 18:53:04 +0530

    update test case for upload service

--- src/upload/upload.service.spec.ts
+++ src/upload/upload.service.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UploadService } from './upload.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UploadService } from "./upload.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { FileEntity } from "src/file/file.entity";
 
-describe('UploadService', () => {
+describe("UploadService", () => {
   let service: UploadService;
+
+  const fileRepository = {
+    update: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UploadService],
+      providers: [
+        UploadService,
+        {
+          provide: getRepositoryToken(FileEntity),
+          useValue: fileRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UploadService>(UploadService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
