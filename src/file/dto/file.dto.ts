202e533c0b47d719f34b32415280721e1209dc1d
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-05 10:25:03 +0530

    get file details

---
+++ src/file/dto/file.dto.ts
@@ -0,0 +1,33 @@
+import { FilterableField, IDField } from "@nestjs-query/query-graphql";
+import { ObjectType, ID, GraphQLISODateTime, Field } from "@nestjs/graphql";
+import { UPLOAD_TYPE } from "src/constants";
+
+@ObjectType("file")
+export class FileDTO {
+  @IDField(() => ID)
+  id!: string;
+
+  @FilterableField()
+  filename!: string;
+
+  @Field()
+  mimetype!: string;
+
+  @Field({ nullable: true })
+  location: string;
+
+  @FilterableField()
+  uploadingStatus!: UPLOAD_TYPE;
+
+  @Field({ nullable: true })
+  size: number;
+
+  @Field({ nullable: true })
+  reasonOfFailure: string;
+
+  @Field(() => GraphQLISODateTime)
+  created!: Date;
+
+  @Field(() => GraphQLISODateTime)
+  updated!: Date;
+}
