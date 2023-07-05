d11684203b50f7ebc881466947ee3dda0845aa2d
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-05 17:53:46 +0530

    unit test cases for file module

--- src/file/file.resolver.spec.ts
+++ src/file/file.resolver.spec.ts
@@ -1,18 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { FileResolver } from './file.resolver';
+import { Test, TestingModule } from "@nestjs/testing";
+import { FileResolver } from "./file.resolver";
+import { FileService } from "./file.service";
 
-describe('FileResolver', () => {
-  let resolver: FileResolver;
+jest.mock("src/config", () => ({
+  SUPPORTED_FILE_FORMETS: ["application/pdf"],
+}));
+
+describe("FileResolver", () => {
+  let fileResolver: FileResolver;
+  let fileService: FileService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FileResolver],
+      providers: [
+        FileResolver,
+        {
+          provide: "FileService",
+          useValue: {
+            uploadFile: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    resolver = module.get<FileResolver>(FileResolver);
+    fileResolver = module.get(FileResolver);
+    fileService = module.get(FileService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should be defined", () => {
+    expect(fileResolver).toBeDefined();
+  });
+
+  it("should return the response from fileService", async () => {
+    const FileInput = {
+      filename: "git-cheatsheet.pdf",
+      mimetype: "application/pdf",
+      encoding: "",
+      createReadStream: jest.fn(),
+    };
+
+    const FileResponse = {
+      fileId: "3dd6d207-066c-42e5-bbed-f40ce7355941",
+      message: "git-cheatsheet.pdf file is uploading...",
+    };
+
+    (fileService.uploadFile as jest.Mock).mockResolvedValue(FileResponse);
+
+    const response = await fileResolver.uploadFile(FileInput);
+    expect(fileService.uploadFile).toBeCalledTimes(1);
+    expect(fileService.uploadFile).toBeCalledWith(FileInput);
+    expect(response).toEqual(FileResponse);
   });
 });
