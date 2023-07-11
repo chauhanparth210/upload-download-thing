87454130cc8dcc808eda00b7f1da68a72e221db9
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-11 16:54:19 +0530

    exclude test coverage from migrations folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,15 +6,3 @@ export const getFilePath = (filename: string): string =>
 
 export const createDirIfNotExists = (dirName) =>
   !fs.existsSync(dirName) ? fs.mkdirSync(dirName) : undefined;
-
-export const bytesToFileSize = (bytes: number, decimals = 0): string => {
-  if (!+bytes) return "0 Bytes";
-
-  const k = 1000;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB"];
-
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
-};
