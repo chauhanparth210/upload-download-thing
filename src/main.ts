2c498bea1cca483fc6c30d605b03163356b70c0d
Parth Chauhan <parth.chauhan@c2fo.com>
2023-07-11 16:28:32 +0530

    call function on project bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "dotenv";
 import { graphqlUploadExpress } from "graphql-upload";
+import { createDirIfNotExists } from "./utils";
+import { BUCKET_DIRECTORY } from "./constants";
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress());
+  createDirIfNotExists(BUCKET_DIRECTORY);
   await app.listen(process.env.APPLICATION_PORT);
 }
 bootstrap();
